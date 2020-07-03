4a4719f87409d4a20a0aa3f62e8da4744bd1e147
AKAI <sasaki.ver1000000@gmail.com>
2020-07-03 22:19:37 +0900

    Add en'clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+class Plan {
+  content = '';
+  strokeDashoffset = 0;
+  strokeDasharray = 100;
+  stroke = '';
+
+  constructor(opt: Partial<Plan> = {}) {
+    opt.stroke = opt.stroke || this.stroke || randomColor();
+    Object.assign(this, opt);
+  }
+}
+
+const randomColor = (h = Math.round(Math.random() * 360)) => `hsl(${h}, ${90}%, ${70}%)`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'enclock';
+  title                     = 'enclock';
+  percentage                = 70;
+  plans: Plan[]             = [new Plan({ content: 'プラン１' })];
+  selectedPlan: null | Plan = null;
+
+  add(): void {
+    this.plans.push(new Plan());
+  }
+
+  division(plan: Plan): void {
+    const strokeDasharray = plan.strokeDasharray / 2;
+    const stroke = randomColor(this.plans.length * 30);
+    const strokeDashoffset = plan.strokeDashoffset - strokeDasharray;
+    plan.strokeDasharray = strokeDasharray;
+    this.plans.push(new Plan({ strokeDasharray, strokeDashoffset, stroke }));
+  }
 }
